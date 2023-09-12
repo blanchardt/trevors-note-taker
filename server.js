16288afd39ae24d1bbfb6f1272d9cbe85feb8ca1
unknown <blancardt@merrimack.edu>
2023-09-12 10:28:33 -0400

    added route to the hotes.html page.

--- server.js
+++ server.js
@@ -17,3 +17,12 @@ app.use(express.static('public'));
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+//GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
