6629bd6d0babc9f36311bb8551a324d6be6c5674
unknown <blancardt@merrimack.edu>
2023-09-12 10:34:08 -0400

    added a post method to add a new note to the db.json file.

--- routes/notes.js
+++ routes/notes.js
@@ -6,4 +6,24 @@ notes.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+//add a new note to the db.json file.
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
 module.exports = notes;
