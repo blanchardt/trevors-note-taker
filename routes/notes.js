ac453ea8b66da4e66baf73db98f7e456d4a2bbfa
unknown <blancardt@merrimack.edu>
2023-09-12 10:32:34 -0400

    Added a git response to get data from the db.json file and added a helper file.

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,9 @@
 const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+//return the data from the db.json file.
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
 module.exports = notes;
